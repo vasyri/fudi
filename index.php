b3d62a3b542578dba3c0afc57c3248af48284522
vasyri <vasylenko99@gmail.com>
2021-05-31 23:56:18 +0300

    fudi QA1

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon">
+
+  <title>FUDI</title>
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="normalize.css">
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+  <!-- header -->
+
+  <header class="header">
+    <div class="header__wrapper content__wrapper">
+      <div class="header__top">
+        <img src="./img/header__logo.png" alt="header__logo">
+        <nav class="header__nav">
+          <a href="#log">LOG IN</a>
+          <a href="#sign">SIGN UP</a>
+          <a href="#toggle" class="header__toggle">&#9776;</a>
+        </nav>
+      </div>
+      <div class="header__middle">
+        <p>Find your Favorite</p>
+        <h1>RECIPES</h1>
+        <div class="header__middle--btn">GET STARTED
+        </div>
+        <p>OR SEND US YOUR OWN RECIPES AND <a class="header__middle-link" href="#"><u>GET&nbsp;REWARDED</u></a>!</p>
+      </div>
+    </div>
+  </header>
+
+  <!-- list  -->
+
+  <section class="list">
+    <div class="list__wrapper content__wrapper">
+      <div class="list__item list__item--item1">
+        <div class="list__img"><img src="./img/list__img1.png" alt="burger"></div>
+        <h2>GET INSPIRED</h2>
+        <div class="list__line"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisc.
+          Pellentesque vel enim a elit viverra elementuma.
+          Aliquam erat&nbsp;volutpat. </p>
+      </div>
+      <div class="list__item list__item--item2">
+        <div class="list__img"><img src="./img/list__img2.png" alt="dollar"></div>
+        <h2>GET REWARDED</h2>
+        <div class="list__line"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisc.
+          Pellentesque vel enim a elit viverra elementuma.
+          Aliquam erat&nbsp;volutpat. </p>
+      </div>
+      <div class="list__item list__item--item3">
+        <div class="list__img"><img src="./img/list__img3.png" alt="shef"></div>
+        <h2>GET SOCIAL</h2>
+        <div class="list__line"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisc.
+          Pellentesque vel enim a elit viverra elementuma.
+          Aliquam erat&nbsp;volutpat. </p>
+      </div>
+    </div>
+  </section>
+
+  <!-- application -->
+  <section class="app">
+    <div class="app__wrapper content__wrapper">
+      <div class="app__img"><img src="./img/app-img.jpg" alt="app-img"></div>
+      <div class="app__text">
+        <h2>INTRODUCING THE FUDI APP</h2>
+        <div class="app__line"></div>
+        <p>Morbi eget posuere dolor. Pellentesque cursus aliquet aliquet. Aeneanet
+          felis sit amet diam mollis ullamcorper. Nullam consequat sem a ante vest
+          ibulum tristique. Suspendisse tristique lacus ac mattis&nbsp;porta. </p>
+        <p>Vivamus ligula quam, vehicula non lacinia sed, faucibus sit amet libero. In
+          libero dui, eleifend eu nisi id, egestas fringilla odio. In varius quam a mas
+          sa hendrerit ullamcorper a eu justo. Suspendisse porta mattis convallis.
+          Aenean tempus ligula ac odio rhoncus, quis aliquam dolor&nbsp;accumsan. </p>
+        <p>Suspendisse aliquet felis consectetur libero congue, sed pulvinar diam
+          malesuada. Duis vehicula a nibh id hendrerit. Donec sit amet ultrices
+          ante, a mattis&nbsp;massa. </p>
+        <div class="app__icons">
+          <div class="app__icon apple"><a href="#"><img src="./img/app__apple.png" alt="apple"></a></div>
+          <div class="app__icon android"><a href="#"><img src="./img/app__android.png" alt="android"></a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- cuisines -->
+
+  <section class="cuisines">
+    <div class="cuisines__wrapper">
+      <h2>BROWSE BY CUISINES</h2>
+      <div class="cuisines__line"></div>
+      <div class="cuisines__items">
+        <div class="cuisines__item cuisines__item--1"><span>327 Recipes</span>
+          <p>ITALIAN</p>
+        </div>
+        <div class="cuisines__item cuisines__item--2"><span>856 Recipes</span>
+          <p>INDIAN</p>
+        </div>
+        <div class="cuisines__item cuisines__item--3"><span>27 Recipes</span>
+          <p>FRENCH</p>
+        </div>
+        <div class="cuisines__item cuisines__item--4"><span>174 Recipes</span>
+          <p>STEAKHOUSE</p>
+        </div>
+        <div class="cuisines__item cuisines__item--5"><span>731 Recipes </span>
+          <p>SEAFOOD</p>
+        </div>
+        <div class="cuisines__item cuisines__item--6"><span>237 Recipes</span>
+          <p>SUSHI</p>
+        </div>
+        <div class="cuisines__item cuisines__item--7"><span>529 Recipes</span>
+          <p>MEXICAN</p>
+        </div>
+        <div class="cuisines__item cuisines__item--8"><span>145 Recipes</span>
+          <p>CHINESE</p>
+        </div>
+        <div class="cuisines__item cuisines__item--9"><span>327 Recipes </span>
+          <p>PIZZA</p>
+        </div>
+        <div class="cuisines__item cuisines__item--10"><span>1.437 Recipes</span>
+          <p>AMERICAN</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- counter  -->
+
+  <section class="counter">
+    <div class="counter__wrapper content__wrapper">
+      <h3>23,567<span>Recipes Available</span>
+      </h3>
+      <h3>431,729<span>Active Users</span>
+      </h3>
+      <h3>892,173<span>Positive Reviews</span>
+      </h3>
+      <h3>56,581<span>Photos & Videos</span>
+      </h3>
+      <h3>3,182<span>Spices and Herbs</span>
+      </h3>
+    </div>
+  </section>
+
+  <!-- footer  -->
+  <footer class="footer">
+    <div class="footer__wrapper content__wrapper">
+      <div class="footer__copyright">
+        &copy;2014 Fudi. All Rights Reserved.
+      </div>
+      <div class="footer__link">
+        DESIGN BY:<br class="br-480px"> <a href="http://WWW.BEKADES.COM">WWW.BEKADES.COM</a>
+      </div>
+    </div>
+  </footer>
+
+</body>
+
+</html>
